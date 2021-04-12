5c26d12bd7127f5ed16bca135c4f3390981aac12
David Hübner <david.huebner@averbis.com>
2021-04-12 13:03:12 +0200

    Apply Apache License Version 2.0

--- src/sos_ruta/kernel.py
+++ src/sos_ruta/kernel.py
@@ -1,6 +1,21 @@
-# Original work: Copyright (c) Bo Peng and the University of Texas MD Anderson Cancer Center
-# Modified work: Copyright 2021 Averbis GmbH
-# Distributed under the terms of the 3-clause BSD License.
+# Copyright 2021 Averbis GmbH
+# This file is part of the sos-ruta project.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# This file incorporates work covered by the following license notice:
+#   Copyright (c) Bo Peng and the University of Texas MD Anderson Cancer Center
+#   Distributed under the terms of the 3-clause BSD License.
 
 import os
 import tempfile
